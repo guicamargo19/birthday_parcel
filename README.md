c28075405f5bc8c29a7e4b0cd9b78ae94e08aa97
camargo <gui.ferreira.camargo@gmail.com>
2024-01-03 14:34:00 -0300

    Página de aniversário com Parcel

---
+++ README.md
@@ -0,0 +1,22 @@
+# Página Contagem regressiva para aniversário
+
+Aplicação de contador regressivo em JavaScript e uso de animação na página.
+
+## Página Web desenvolvida no curso de Full Stack Python na EBAC - Escola Britânica de Artes Criativas e Tecnologia
+
+### Ferramentas utilizadas
+
+<ul>
+    <li><b>HTML5 e CSS3</b></li><br>
+    <p>Elementos HTML utlizados de forma semântica e estilizados com CSS.</p>
+    <li><b>JavaScript</b></li><br>
+    <p>Aplicação de JS para manusear a contagem regressiva.</p>
+    <li><b>Bootstrap Icons</b></li><br>
+    <p>Ícones utilizados para mídias sociais no rodapé.</p>
+    <li><b>SASS</b></li><br>
+    <p>Utilização de SASS, linguagem de extensão CSS.</p>
+    <li><b>Parcel</b></li><br>
+    <p>Boas práticas de programação com metodologia BEM visando melhor manutenção do código.</p>
+</ul>
+    
+    
---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+AOS.init();
+
+const dataDoEvento = new Date("Feb 2, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distaciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distaciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distaciaAteOEvento % diaEmMs) / horaEmMs);
+    const minutosAteOEvento = Math.floor((distaciaAteOEvento % horaEmMs) / minutoEmMs);
+    const segundosAteOEvento = Math.floor((distaciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distaciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('hero-text').innerHTML = 'A comemoração de aniversário chegou!';
+        document.getElementById('contador').innerHTML = '';
+    }
+}, 1000);
